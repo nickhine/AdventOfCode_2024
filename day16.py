0f81dd47a6c8d7aab152abc696d2e539ff1a8ff9
Nicholas Hine <n.d.m.hine@warwick.ac.uk>
2024-12-16 23:09:41 +0000

    version of day16.py that runs in 20s

--- day16.py
+++ day16.py
@@ -2,7 +2,7 @@ import numpy as np
 
 mapl = []
 instr = ""
-with open('t16b.dat') as f:
+with open('d16.dat') as f:
     while True:
         line = f.readline().strip()
         if not line:
@@ -29,7 +29,6 @@ for i in range(ly):
         if mapl[i][j] == 'E':
             endpos[0] = j
             endpos[1] = i
-#print('stpos,endpos=',stpos,endpos)
 
 def print_map(mapb,pos,path,npath):
     mapstr = ""
@@ -48,8 +47,6 @@ def print_map(mapb,pos,path,npath):
                 index = np.where((path[0:npath]==(j,i)).all(axis=1))[0][0]
                 if index > 0:
                     prev = path[index-1]
-                    #print(index,path[0:npath])
-                    #print(path[index],prev)
                     mdir = np.where((dp==path[index]-prev).all(axis=1))[0]
                     if len(mdir) > 0:
                         mapstr += dirch[mdir[0]]
@@ -62,14 +59,12 @@ def print_map(mapb,pos,path,npath):
     print(mapstr,end='' )
 
 def turn_cost(dir,pdir):
+    cost = 0
     if dir != pdir:
         x = abs(dir-pdir)
         x = x if x<3 else 1
-        score = x*1000
-        #print(pos,endpos,'Turn',dirch[pdir],'->',dirch[dir],'score:',score)
-    else:
-        score = 0
-    return score
+        cost = x*1000
+    return cost
 
 dirch = ['>','v','<','^']
 dp = [(1,0),(0,1),(-1,0),(0,-1)]
@@ -90,75 +85,66 @@ ldir = 0
 pdir = 0
 score = 0
 worstbt = 0
-bestnpath = 100000
-bestscore = 100000
+bestnpath = 1000000
+bestscore = 1000000
 bestpath = np.zeros((maxpath,2),dtype=int)
 while True:
     cdirs[:] = 0
-    for i in range(ldir,4):
-        npos = pos + dp[i]
+    ddirs = [pdir,(pdir-1)%4,(pdir+1)%4] #[0,1,2,3]
+    for idir in ddirs: #range(ldir,4):
+        if ddirs.index(idir) < ddirs.index(dir):
+            continue
+        npos = pos + dp[idir]
         if mapa[npos[1],npos[0]] == 1:
             continue
-        #if pos[0] == 15 and pos[1] == 7 and score<11090:
-        #    print('Checking pos',pos,'i',i,'dir',dir,'npos',npos,'mapbest',mapbest[npos[1],npos[0]],'score',score,turn_cost(i,dir),'cdirs',cdirs)
-        if score + turn_cost(i,dir) > mapbest[npos[1],npos[0],i]:
+        if score + turn_cost(idir,dir) > mapbest[npos[1],npos[0],idir]:
             continue
         pathcheck = (path[0:npath]==npos).all(axis=1).any()
-        #print(i,npos,path[0:npath],pathcheck)
-        cdirs[i] = (mapa[npos[1],npos[0]] != 1 and not pathcheck)
+        cdirs[idir] = (mapa[npos[1],npos[0]] != 1 and not pathcheck)
     if np.sum(cdirs) > 1:
         skipped1st = False
-        #for dm in range(4): 
-        for dm in [0,1,2,3]:
+        for dm in ddirs:
             d = dm%4
             if cdirs[d]:
                 if not skipped1st:
+                    #print('skipped: ',dir,'->',d)
                     skipped1st = True
                 else:
                     nodes.append((path[0:npath].copy(),d,pdir,score))
-                    #print('adding node ',len(nodes),' at pos',pos,'npath',npath,'dir',d,'pdir',pdir,'score',score)
-        #print_map(mapa,pos,path,npath)
-        #print("Intersection at ",pos,": available dirs:",cdirs)
     if np.sum(cdirs) == 0:
         if len(nodes) == 0:
             print('Dead end and no more nodes')
             break
-        lp,ldir,pdir,score = nodes.pop(0)
-        dir = ldir
+        lp,dir,pdir,score = nodes.pop(0)
+        ldir = dir
         npath = len(lp)
         path[0:npath] = lp[0:npath]
         pos[:] = path[npath-1][:]
-        #print('Backtracking to:',pos,'ldir:',ldir,'path length:',npath,'score:',score)
-        #print_map(mapa,pos,path,npath)
         continue
-    #for ndir in range(4):
-    for ndir in [0,1,2,3]:
+    for ndir in ddirs:
         mdir = ndir%4
         if cdirs[mdir]:
             dir = mdir
             break
+    #print('turning: ',pdir,'->',dir)
     score += turn_cost(dir,pdir)
     pdir = dir
     npos = pos + dp[dir]
     score += 1
     ldir = 0
     pos[:] = npos[:]
-    #print('pos=',pos,'dir=',dirch[dir],'score=',score)
     if score < mapbest[pos[1],pos[0],dir]:
         mapbest[pos[1],pos[0],dir] = score
     path[npath][:] = pos[:]
     npath += 1
     if (pos==endpos).all():
-        #print("End, score =",score)
         if score == bestscore:
-            print("Equal best score:",score,' (nodes=',len(nodes),')')
-            #if bestscore<91000:
-            #    print_map(mapa,pos,path,npath)
+            #print("Equal best score:",score,' (nodes=',len(nodes),')')
             for i in range(npath):
                 onbest[path[i][1],path[i][0]] = 1
         if score < bestscore:
             print("New best score:",score,' (nodes=',len(nodes),')')
-            print_map(mapa,pos,path,npath)
+            #print_map(mapa,pos,path,npath)
             onbest[:,:] = 0
             for i in range(npath):
                 onbest[path[i][1],path[i][0]] = 1
@@ -168,12 +154,12 @@ while True:
         if len(nodes) == 0:
             print('No more nodes')
             break
-        lp,ldir,pdir,score = nodes.pop(0)
+        lp,dir,pdir,score = nodes.pop(0)
+        ldir = dir
         npath = len(lp)
         path[0:npath] = lp
         pos[:] = path[npath-1][:]
         continue
-    #print_map(mapa,pos,path,npath)
 
 print("\n\n\nBest path:",bestnpath,bestscore)
 print_map(mapa,endpos,bestpath,bestnpath)
