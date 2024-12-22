505c8bf23ba2ec595e0468bed2bff33f77c6ba6d
Nicholas Hine <n.d.m.hine@warwick.ac.uk>
2024-12-22 14:52:17 +0000

    Final simplified day22.py

--- day22.py
+++ day22.py
@@ -1,6 +1,5 @@
 import numpy as np
 np.set_printoptions(threshold=np.inf,linewidth=np.inf)
-
 secrets = []
 with open('d22.dat') as f:
     while True:
@@ -22,14 +21,12 @@ for i in range(-9,9):
                 combinations.append((i,j,k,l))
 print('combinations=',len(combinations))
 combinations = np.array(combinations)
-
 maxtime = 2000
 secrets = np.array(secrets)
 buyers = len(secrets)
 prices = np.zeros((buyers,maxtime),dtype=int)
 oldprices = np.zeros((len(secrets)),dtype=int)
 seqint = -np.ones((buyers,maxtime),dtype=int)
-
 for time in range(maxtime):
     prices[:,time] = secrets % 10
     sec0 = secrets
@@ -43,22 +40,13 @@ for time in range(maxtime):
     seqint[:,time] = (sequences[:,0]+9)*19**3+(sequences[:,1]+9)*19**2+(sequences[:,2]+9)*19**1+(sequences[:,3]+9)*19**0
 print('Sum of secrets:',sum(secrets))
 maxbananas = 0
-bestcomb = -1
-for ic in range(combinations.shape[0]):
-    comb = combinations[ic,:]
-    seq = (comb[0]+9)*19**3+(comb[1]+9)*19**2+(comb[2]+9)*19**1+(comb[3]+9)*19**0
-    bananas = 0
-    #print(f'testing combination {ic}: {comb} {seq} progress: {ic/combinations.shape[0]*100:5.3f}%')
-    mask = (seqint[:,:] == seq)
-    first_time = np.argmax(mask,axis=-1)
-    first_time = np.where(mask.any(axis=-1),first_time,-1)
-    #print(prices[np.arange(buyers),first_time])
-    #print('prices:',prices[np.arange(buyers),first_time]*(first_time >= 0))
-    bananas += np.sum(prices[np.arange(buyers),first_time]*(first_time >= 0))
-    if bananas > maxbananas:
-        maxbananas = bananas
-        bestcomb = comb
-print(f'new maxbananas: {maxbananas} with combination {bestcomb}')
+all_seqs = (combinations[:,0]+9)*19**3+(combinations[:,1]+9)*19**2+(combinations[:,2]+9)*19**1+(combinations[:,3]+9)*19**0
+chunk_size = 1
+for i in range(0, len(all_seqs), chunk_size):
+    masks = (seqint[:,:,None] == all_seqs[i:i + chunk_size])
+    first_time = np.where(masks.any(axis=1),np.argmax(masks,axis=1),-1)
+    maxbananas = max(maxbananas, np.max(np.sum(prices[np.arange(buyers)[:,None],first_time]*(first_time >= 0), axis=0)))
+print(f'maxbananas: {maxbananas}')
 
 
             
